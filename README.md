8aa89314ad90cd489fb2ca914b63641bee091cd3
ammr2 <ammar.elshabrawy@yahoo.com>
2021-11-30 20:03:14 +0200

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # notifications-service
+
 service to handle notifications
+
+# Pre-requisites
+
+- Install [docker](https://www.docker.com)
+
+# Getting started
+
+- Clone the repository
+
+```
+git clone  <git lab template url> <project_name>
+```
+
+### Running the build
+
+All the different build steps are orchestrated via [npm scripts](https://docs.npmjs.com/misc/scripts).
+Npm scripts basically allow us to call (and chain) terminal commands via npm.
+
+| Npm Script | Description                                                                       |
+| ---------- | --------------------------------------------------------------------------------- |
+| `start`    | Runs full build docker image `docker-compose up --build -d && docker attach api ` |
+| `dev`      | Runs full build before starting all watch tasks. Can be invoked with              |
+| `bye`      | docker-compose down                                                               |
+| `test`     | Runs build and run tests using mocha                                              |
+| `coverage` | Runs coverage on project fils                                                     |
+| `lint`     | Runs TSLint on project files                                                      |
+
+Navigate to `http://localhost:5000`
+
+- API Document endpoints
+
+  swagger-ui Endpoint : http://localhost:5000/docs
--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ const compression = require('compression');
 const swaggerUi = require('swagger-ui-express');
 const morgan = require('morgan');
 const newrelic = require('newrelic');
-const permit = require('./routes/middleware/permission');
 
 const { mongo } = require('./db');
 const swaggerDocument = require('./routes/docs.json');
@@ -16,25 +15,12 @@ const swaggerDocument = require('./routes/docs.json');
 module.exports = async function initApp(deps) {
   const {
     authMiddleware,
-    districtRoutes,
-    cityRoutes,
-    userRoutes,
-    requestRoutes,
     notificationRoutes,
-    roomRoutes,
-    smsRoutes,
     config,
-    fileRoutes,
-    adRoutes,
-    couponRoutes,
     crons,
     redisClient,
     requestModel,
     notificationService,
-    paymentRoutes,
-    targetedUsersRoutes,
-    subscriptionPackageRoute,
-
   } = deps;
   const cronInstance = crons.instance(redisClient);
   try {
@@ -45,6 +31,7 @@ module.exports = async function initApp(deps) {
     console.error(err);
     process.exit(1);
   }
+  
   if (config.runCrons) {
     const task = crons.notifications({ instance: cronInstance, requestModel, notificationService });
     task.start();
@@ -55,11 +42,15 @@ module.exports = async function initApp(deps) {
     if (req.url.includes('/api')) req.url = req.url.replace(/\/api/gi, '');
     next();
   });
+
+
   app.use(compression());
   app.use(cors({ exposedHeaders: ['authorization'] }));
   app.use(bodyParser.json());
   app.use(cookieParser());
   app.use(bodyParser.urlencoded({ extended: false }));
+
+
   // eslint-disable-next-line no-unused-vars
   app.use('/', express.Router().get('/', (req, res, next) => res.status(200).end()));
   if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
@@ -69,23 +60,11 @@ module.exports = async function initApp(deps) {
   }
   app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use(authMiddleware.checkToken);
-  app.use('/city', cityRoutes);
-  app.use('/district', districtRoutes);
-  app.use('/user', userRoutes);
-  app.use('/request', requestRoutes);
+ 
   app.use('/notifications', notificationRoutes);
-  app.use('/rooms', roomRoutes);
-  app.use('/file', fileRoutes);
-  app.use('/admin', permit(['ADMIN', 'CUSTOMER_SUPPORT']), smsRoutes);
-  app.use('/ad', adRoutes);
-  app.use('/coupon', couponRoutes);
-  app.use('/payment', paymentRoutes);
-  app.use('/targeted-users', targetedUsersRoutes);
-  app.use('/subscribe-package', subscriptionPackageRoute);
+ 
 
-  /* please don't remove next param it's used by the error handling lib
-   * it's consumed 4 hours debugging
-   */
+  /* error handling lib  */
   // eslint-disable-next-line no-unused-vars
   app.use((err, req, res, next) => {
     if (!err.status && err.message !== 'validation Failed') {
